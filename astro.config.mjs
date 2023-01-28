2dad03088bdfa897966276ff1347ae808f0a43b5
Joshua Miller <16845458+kaosine@users.noreply.github.com>
2023-01-27 21:51:54 -0500

    Astro setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import { SITE } from './src/config';
-
 import sitemap from '@astrojs/sitemap';
+import svelte from "@astrojs/svelte";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte(), prefetch()],
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0'
   },
-  site: SITE.url,
+  site: SITE.url
 });
